bbe82bccbe757264c7ac49932e5a92f167cdf254
tonysnickers <antoine.mendy@hotmail.fr>
2022-11-07 16:53:30 +0100

    card stylised

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({setMovie, movie}) => {
 
-  
+
   return (
     <div className="search-form">
       <h1>react Movie</h1>
-      <form className="form">
-        <input type="text" />
+
+        <input
+        type="text"
+        onChange={(e) => setMovie(e.target.value)}
+        defaultValue={movie}
+        />
         <input type="submit" />
-      </form>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,29 +8,36 @@ import Search from '../components/Search'
 
 const Home = () => {
   const [data, setData] = useState([])
+  const [movie, setMovie] = useState('')
 
   useEffect(() => {
-    const movieFect = () => {
       axios(
-        'https://api.themoviedb.org/3/search/movie?api_key=7a6a1bf84273ea2287a836a3821ac0a7&query=code&language=fr-FR',
+        'https://api.themoviedb.org/3/search/movie?api_key=7a6a1bf84273ea2287a836a3821ac0a7&query=dance&language=fr-FR',
+      ).then((res) => setData(res.data.results))
+
+  }, [])
+
+  useEffect(() => {
+    const searchMovie = () => {
+      axios(
+        `https://api.themoviedb.org/3/search/movie?api_key=7a6a1bf84273ea2287a836a3821ac0a7&query=${movie}&language=fr-FR`,
       ).then((res) => setData(res.data.results))
     }
-    console.log(data)
-    movieFect()
-  }, [])
+    searchMovie()
+  }, [movie])
+
   return (
     <div className="home">
       <div className="header">
         <Nav />
-        <Search />
+        <Search setMovie={setMovie} movie={movie} />
       </div>
 
       <Container sx={{ marginTop: '60px' }}>
-        <Grid container spacing={2}>
+        <Grid container spacing={3}>
           {data.map((movie) => (
-            <Grid item xs={4} key={movie.id}>
+            <Grid item xs={3} key={movie.id}>
               <CardMovie movie={movie} />
-
             </Grid>
           ))}
         </Grid>
